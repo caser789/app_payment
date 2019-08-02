a332e1f1b0097ff976e29a0809553036d81e809e
Xue Jiao <jiao.xue@shopee.com>
2019-08-02 16:50:39 +0800

    Run the server

--- app_payment/server.py
+++ app_payment/server.py
@@ -1,1 +1,13 @@
+from dock.web import DockApp
 
+app = DockApp("")
+
+from dock.common import config
+
+@app.flaskapp.route('/')
+def health():
+    return 'OK'
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ tests/view/transaction.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class CreateTransactionTestCase(unittest.TestCase):
+
+    def test_ok(self):
+        pass
+
+    def test_provision_not_belong_to_same_transaction_then_error(self):
+        pass
+
+    def test_transaction_exists(self):
+        pass
